4e0fc7760a45cb9e3aae49f5af58a26cde950358
ekaterina <chvetochek@list.ru>
2025-01-08 18:03:09 +0300

    дз 12 готово

--- hw12/build.gradle.kts
+++ hw12/build.gradle.kts
@@ -1,4 +1,20 @@
 dependencies {
+    implementation("ch.qos.logback:logback-classic")
+    implementation("com.google.code.gson:gson")
+
+    implementation("org.eclipse.jetty.ee10:jetty-ee10-servlet")
+    implementation("org.eclipse.jetty:jetty-server")
+    implementation("org.eclipse.jetty.ee10:jetty-ee10-webapp")
+    implementation("org.eclipse.jetty:jetty-security")
+    implementation("org.eclipse.jetty:jetty-http")
+    implementation("org.eclipse.jetty:jetty-io")
+    implementation("org.eclipse.jetty:jetty-util")
+    implementation("org.freemarker:freemarker")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.mockito:mockito-junit-jupiter")
     implementation ("org.projectlombok:lombok")
     annotationProcessor ("org.projectlombok:lombok")
 
---
+++ hw12/src/main/java/ru/otus/server/ClientsWebServerSimple.java
@@ -0,0 +1,103 @@
+package ru.otus.server;
+
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.hibernate.cfg.Configuration;
+import ru.otus.core.repository.DataTemplateHibernate;
+import ru.otus.core.repository.HibernateUtils;
+import ru.otus.core.sessionmanager.TransactionManagerHibernate;
+import ru.otus.crm.dbmigrations.MigrationsExecutorFlyway;
+import ru.otus.crm.model.Address;
+import ru.otus.crm.model.Client;
+import ru.otus.crm.model.Phone;
+import ru.otus.crm.service.DbServiceClientImpl;
+import ru.otus.helpers.FileSystemHelper;
+import ru.otus.services.TemplateProcessor;
+import ru.otus.servlet.ClientsServlet;
+
+import java.util.List;
+
+import static ru.otus.demo.DbServiceDemo.HIBERNATE_CFG_FILE;
+
+public class ClientsWebServerSimple implements ClientsWebServer {
+    private static final String START_PAGE_NAME = "index.html";
+    private static final String COMMON_RESOURCES_DIR = "static";
+
+    protected final TemplateProcessor templateProcessor;
+    private final Server server;
+
+    public ClientsWebServerSimple(int port, TemplateProcessor templateProcessor) {
+        this.templateProcessor = templateProcessor;
+        server = new Server(port);
+    }
+
+    @Override
+    public void start() throws Exception {
+        if (server.getHandlers().isEmpty()) {
+            initContext();
+        }
+        server.start();
+    }
+
+    @Override
+    public void join() throws Exception {
+        server.join();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        server.stop();
+    }
+
+    private void initContext() {
+
+        ResourceHandler resourceHandler = createResourceHandler();
+        ServletContextHandler servletContextHandler = createServletContextHandler();
+
+        Handler.Sequence sequence = new Handler.Sequence();
+        sequence.addHandler(resourceHandler);
+        sequence.addHandler(applySecurity(servletContextHandler, "/clients"));
+
+        server.setHandler(sequence);
+    }
+
+    @SuppressWarnings({"squid:S1172"})
+    protected Handler applySecurity(ServletContextHandler servletContextHandler, String... paths) {
+        return servletContextHandler;
+    }
+
+    private ResourceHandler createResourceHandler() {
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirAllowed(false);
+        resourceHandler.setWelcomeFiles(START_PAGE_NAME);
+        resourceHandler.setBaseResourceAsString(
+                FileSystemHelper.localFileNameOrResourceNameToFullPath(COMMON_RESOURCES_DIR));
+        return resourceHandler;
+    }
+
+    private ServletContextHandler createServletContextHandler() {
+        var configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
+
+        var dbUrl = configuration.getProperty("hibernate.connection.url");
+        var dbUserName = configuration.getProperty("hibernate.connection.username");
+        var dbPassword = configuration.getProperty("hibernate.connection.password");
+
+        new MigrationsExecutorFlyway(dbUrl, dbUserName, dbPassword).executeMigrations();
+
+        var sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class, Address.class, Phone.class);
+
+        var transactionManager = new TransactionManagerHibernate(sessionFactory);
+        ///
+        var clientTemplate = new DataTemplateHibernate<>(Client.class);
+        ///
+        var dbServiceClient = new DbServiceClientImpl(transactionManager, clientTemplate);
+        dbServiceClient.saveClient(new Client("Client1"));
+        var clientSecond = dbServiceClient.saveClient(new Client(null, "Client2", new Address(null, "Lenina"), List.of(new Phone(null, "232425"), new Phone(null, "123123"))));
+        ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletContextHandler.addServlet(new ServletHolder(new ClientsServlet(templateProcessor, dbServiceClient)), "/clients");
+        return servletContextHandler;
+    }
+}
