1c9b1b8b7e8a00f8ea6f3d135f7764a1364c0260
ekaterina <chvetochek@list.ru>
2025-01-21 01:15:26 +0300

    дз 14 черновик

--- hw14/src/main/resources/db/migration/V1__initial_schema.sql
+++ hw14/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,12 +1,12 @@
 create table address
 (
-    id   bigint not null primary key,
+    id   bigserial not null primary key,
     street varchar(50)
 );
 
 create table client
 (
-    id   bigint not null primary key,
+    id   bigserial not null primary key,
     name varchar(50),
     address_id   bigint,
     constraint fk_user_account_address_id foreign key (address_id) references address (id)
@@ -19,6 +19,6 @@ create table client
 --    client_id bigint
 --);
 
- INSERT INTO address (id, street) VALUES (1, 'moscow');
- INSERT INTO client (id, name) VALUES (1, 'kate1');
-  INSERT INTO client (id, name, address_id) VALUES (2, 'kate2', 1);
+-- INSERT INTO address (id, street) VALUES (1, 'moscow');
+-- INSERT INTO client (id, name) VALUES (1, 'kate1');
+-- INSERT INTO client (id, name, address_id) VALUES (2, 'kate2', 1);
